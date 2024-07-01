ec9217db5375b279efee13307fb46299d6280d55
Danica Gazdić <danicagazdic@gmail.com>
2024-07-01 22:37:17 +0200

    Finished scaling experiments

--- python/main.py
+++ python/main.py
@@ -54,9 +54,70 @@ def compute_mandelbrot(max_iter, mode, w, h, processes, xmin, xmax, ymin, ymax):
     plt.savefig(f"../data/python/{mode}.png")
     # plt.show()
 
+
+def strong_scaling():
+    # processes = [1, 2, 4, 6, 8, 12, 16, 18]
+    processes = [12]
+    max_iter = 1000
+    w = 800
+    h = 500
+    xmin = -2.5
+    xmax = 1.0
+    ymin = -1.0
+    ymax = 1.0
+    mean_times = []
+    std_devs = []
+    for i in range(len(processes)):
+        times = []
+        for _ in range(30):
+            start_time = time.time()
+            compute_mandelbrot(max_iter, "parallel", w, h, processes[i], xmin, xmax, ymin, ymax)
+            end_time = time.time()
+            times.append(end_time - start_time)
+            print(f"Execution time: {end_time - start_time:.4f} seconds")
+        mean_times.append(np.mean(times))
+        std_devs.append(np.std(times))
+        print(f"Num of processes: {processes[i]}\tAverage time: {np.mean(times):.4f} seconds\tStd dev: {np.std(times):.4f} seconds")
+
+    print(mean_times)
+    print(std_devs)
+
+def weak_scaling():
+    # processes = [1, 2, 4, 6, 8, 12, 16, 18]
+    max_iter = 1000
+    w = 200
+    h = [100, 200, 400, 600, 800, 1200, 1600, 1800]
+    xmin = -2.5
+    xmax = 1.0
+    ymin = -1.0
+    ymax = 1.0
+    mean_times = []
+    std_devs = []
+    for i in range(len(processes)):
+        times = []
+        for _ in range(10):
+            start_time = time.time()
+            compute_mandelbrot(max_iter, "parallel", w, h[i], processes[i], xmin, xmax, ymin, ymax)
+            end_time = time.time()
+            times.append(end_time - start_time)
+            print(f"Execution time: {end_time - start_time:.4f} seconds")
+            time.sleep(1)
+        mean_times.append(np.mean(times))
+        std_devs.append(np.std(times))
+        print(f"Num of processes: {processes[i]}\tAverage time: {np.mean(times):.4f} seconds\tStd dev: {np.std(times):.4f} seconds")
+
+    print(mean_times)
+    print(std_devs)
+
 if __name__ == "__main__":
     start_time = time.time()
     mode = sys.argv[1] if len(sys.argv) > 1 else "serial"
+    if(mode == "strong"):
+        strong_scaling()
+        exit(0)
+    elif(mode == "weak"):
+        weak_scaling()
+        exit(0)
     max_iter = int(sys.argv[2]) if len(sys.argv) > 2 else 1000
     w = int(sys.argv[3]) if len(sys.argv) > 3 else 800
     h = int(sys.argv[4]) if len(sys.argv) > 4 else 500
